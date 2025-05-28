afa1e7225421ce8f96dbdad430f0e67e92b4e094
KaidenJosephKeane <kaiden.keane@stu.vertexacademies.org>
2025-05-28 18:13:21 +0000

    Finished 3.2.1

--- 321_listls_KEANE.py
+++ 321_listls_KEANE.py
@@ -1,1 +1,18 @@
-print("Hello world!)
+print("Hello World!")
+# a list of menu items
+food = ["Burger", "Shake", "Fries"]
+
+print(food[0], "is located at index 0.")
+print(food[1], "is located at index 1.")
+print(food[2], "is located at index 2.")
+print(food[-3], "is located at index -3.")
+
+  if("Sundae" in food):
+    print("You can have a sunade!")
+  else:
+    print("We don't have sundaes.")
+  
+if("Chicken" in food):
+    print("You can have Chicken")
+
+else 
